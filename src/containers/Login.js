43780de5eb1f504ceb8a7ee4311681d78c508272
rudzbart <rudzbartosz97@gmail.com>
2020-01-22 22:00:12 +0100

    poprawki login

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,34 +1,40 @@
 import React, {useState} from "react";
 import {Button, FormGroup, FormControl} from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, Redirect, withRouter } from "react-router-dom";
+import Home from "./Home";
 import "./Login.css";
 
 export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  var toHome = 0;
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-
+  function handleSubmit(event){
+    if(email == "rudzbartosz97@gmail.com" && password == "bartek"){
     try{
       alert("Logged in");
-      props.userHasAuthenticated(true);
-    }
+      toHome = 1;
+      }
     catch(e){
       alert(e.message);
-
+      }
+    }
+    else {
+      alert("Your email or password is incorrect!");
     }
   }
 
+  if(toHome == 1)return(<Redirect to='/home'/>);
+
   return (
     <div className="Login">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={e => handleSubmit(e.target.value)}>
         <FormGroup controlId="email">
-          <h1>Witaj!</h1>
+          <h1>Welcome!</h1>
           <FormControl
             autoFocus
             type="email"
@@ -45,13 +51,10 @@ export default function Login(props) {
             type="password"
           />
         </FormGroup>
-        <Link to="/home">
-        <Button block disabled={!validateForm()} type="submit">
+        <Button id="login-button" block disabled={!validateForm()} type="submit">
           Login
         </Button>
-        </Link>
       </form>
-      <div><p className="incorrectpass">podano błędne hasło</p></div>
     </div>
   );
 
